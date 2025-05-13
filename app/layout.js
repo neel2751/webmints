a807bdac2ed7c018edaba852c9f24a3c969fb87a
Neel Patel <neel@Neels-MBP.lan>
2025-05-13 17:59:27 +0100

    Add the google tag manager

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-import { Inter, Space_Grotesk, Work_Sans } from "next/font/google";
+import { Space_Grotesk, Work_Sans } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Work_Sans({
@@ -21,6 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <GoogleTagManager gtmId="GTM-M79C75BG" />
       <body className="antialiased font-sans">{children}</body>
     </html>
   );
